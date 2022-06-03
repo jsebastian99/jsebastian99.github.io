fee6044a7cd520d70daa09155b0dc8b371bab8bc
jsebastian99 <jsaldana.ramirez99@gmail.com>
2022-06-02 23:08:25 -0500

    Subir Website
    He subido todos los archivos.

---
+++ js/questions.js
@@ -0,0 +1,24 @@
+(function(){
+
+    const titleQuesitons = [...document.querySelectorAll('.questions_title')];
+
+    console.log(titleQuesitons)
+
+    titleQuesitons.forEach(question =>{
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle('questions__padding--add');
+            question.children[0].classList.toggle('questions__arrow--rotate');
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`;
+
+        });
+    });
+})();
